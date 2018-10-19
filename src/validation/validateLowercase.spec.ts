fbd2cc11f56670ed600b5c72fb15cbed9cafe3c4
Cai Leao <caina.leao@endemolshine.com>
2018-10-19 12:01:59 +0100

    fix(validation): fix for subject and lowercase validation
    Question validation now uses previous answers to determine maximum length for
    input and all answers are checked for lowercase only

---
+++ src/validation/validateLowercase.spec.ts
@@ -0,0 +1,13 @@
+import validateLowercase from './validateLowercase';
+
+describe('#validateLowercase', () => {
+  it("prevents commits if there's a capital letter", () => {
+    expect(validateLowercase('Test')).toEqual(
+      'The value must be all lowercase',
+    );
+  });
+
+  it('allows commits if the value is all lowercase', () => {
+    expect(validateLowercase('a value all lowercase')).toBeTruthy();
+  });
+});
--- src/validation/validateSubject.spec.ts
+++ src/validation/validateSubject.spec.ts
@@ -2,21 +2,25 @@ import validateSubject from './validateSubject';
 
 describe('#validateSubject', () => {
   it('prevents commits if no subject specified', () => {
-    expect(validateSubject()).toContain('specify subject');
+    expect(
+      validateSubject('', { type: 'feat', scope: 'a custom scope' }),
+    ).toContain('specify subject');
   });
 
   it('prevents commits if subject is greater than 70 characters', () => {
     expect(
       validateSubject(
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut',
+        { type: 'feat', scope: 'a custom scope' },
       ),
-    ).toContain('Subject should be 72 characters or less');
+    ).toContain('characters or less');
   });
 
   it('allows commits if subject passes all conditions', () => {
     expect(
       validateSubject(
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do',
+        { type: 'feat', scope: 'a custom scope' },
       ),
     ).toBeTruthy();
   });
