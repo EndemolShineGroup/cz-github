197e8818aa3443e187fe550bed7cdc73713f64fc
Cai Leao <caina.leao@endemolshine.com>
2018-11-19 13:57:47 +0000

    refactor(formatting): refactor code and fix tests
    Refactored `formatCommit()` to reduce code complexity and fixed test
    expectations to match the input received from CLI.

--- src/lib/formatCommit.ts
+++ src/lib/formatCommit.ts
@@ -1,7 +1,6 @@
 import os from 'os';
 
 import { Answers } from 'inquirer';
-import compact from 'lodash.compact';
 import wrap from 'word-wrap';
 
 import { splitIssues } from './util';
@@ -24,28 +23,42 @@ export default function formatCommit(answers: Answers) {
     type = 'feat',
   } = answers;
 
+  console.log('ANSWERS', answers);
+
+  let messageArr: string[] = [];
+
+  // header
+  messageArr.push(getHeader(type, scope, subject));
+  // body
+  if (body !== '') {
+    messageArr.push(getBody(body, isBreakingChange));
+  }
+
+  if (isIssueAffected) {
+    messageArr.push(formattedIssues(issues));
+  }
+
+  return messageArr.join(os.EOL + os.EOL);
+}
+
+const getHeader = (type: string, scope: string, subject: string): string => {
+  scope = scope === '' ? scope : `(${scope})`;
   // Hard limit this line
-  const commitHeader = `${type}${
-    scope ? `(${scope})` : ''
-  }: ${subject.trim()}`.slice(0, wrapOptions.width);
+  return `${type}${scope}: ${subject.trim()}`.slice(0, wrapOptions.width);
+};
 
+const getBody = (body: string, isBreakingChange: boolean): string => {
+  body = isBreakingChange ? `BREAKING CHANGE: ${body}` : body;
   // Wrap these lines at 100 characters
-  const commitBody = wrap(
-    `${isBreakingChange ? 'BREAKING CHANGE: ' : ''}${body ? body : ''}`,
-    wrapOptions,
-  );
+  return wrap(body, wrapOptions);
+};
 
-  // GitHub issue IDs
-  const formattedIssues = splitIssues(issues)
+// GitHub issue IDs
+const formattedIssues = (issues: string) => {
+  return splitIssues(issues)
     .map((issue, index) => {
-      // @TODO Make this better
-      return index === 0 ? `Fixes ${issue}` : `fixes ${issue}`;
+      let prefix = index === 0 ? 'Fixes' : 'fixes';
+      return `${prefix} ${issue}`;
     })
     .join(', ');
-
-  const commitFooter = isIssueAffected ? `${formattedIssues}` : '';
-
-  const message = compact([commitHeader, commitBody, commitFooter]);
-
-  return message.join(os.EOL + os.EOL);
-}
+};
